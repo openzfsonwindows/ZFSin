openzfsonosx/spl

Help ABD reduce fragmentation (#15)
Scattered ABD is very "sticky"; on multi-cpu machines a
large number of kmem cache magazines accumulate so allocs
and frees are serviced from the abd_chunk kmem cache --
allocs and frees rarely descend to the bucket layer.

* Introduce kmem_cache_free_to_slab, sysctl-tunable, default off.

It defaults off because it increases kmem_free() latency
both by running extra code (rather than updating a
non-full magazine in the typical case) and lock contention
on the arena mutex.   It rapidly reduces fragmentation in
the trees rooted at the heap and the zfs_qcache arenas,
which under some loads (lots of metadata updating, in
particular) can become highly fragmented.

When on, kmem_free() calls (to caches whose vmem arenas
can maintain excess memeory) will test whether there is
apparent fragmentation in the cache's parent arena and in
the system generally.  When there is such fragmentation,
frees will go the the slab layer rather than the depot
layer.

We keep the fast path mechanism of freeing to the loaded
cpu magazine until the magazine is full.

If the loaded magazine is full, we swap loaded with last-loaded
magazine as usual if there is no fragmentation in the
parent arena.

However, if the loaded magazine is full and the parent
arena is fragmented, we return the memory to the slab
layer.

This a shortcut of the (working-set dependent) release of
full magazines from the depot layer at reap time.

The thresholds are sufficiently high that small, dense, or
busy kmem_caches will rarely take this path.

* Expose kmem_depot_ws_zero(), so it can be called when
there has been successful abd move, as moving tends to
create large numbers of full magazines which would linger
in the depot layer as an unnecessarily inflated working
set.   The working set will grow as usual if the abd cache
is busy between the _zero() call and the next reap, so
this mainly affects moves that release memory while the
system is quiescent.

* New flag, KMC_ARENA_SLA

include/sys/kmem.h
@@ -98,6 +98,7 @@ extern uint64_t physmem;
 #define KMC_KMEM_ALLOC  0x00200000      /* internal use only */
 #define KMC_IDENTIFIER  0x00400000      /* internal use only */
 #define KMC_PREFILL     0x00800000
+#define KMC_ARENA_SLAB  0x01000000      /* use a bigger kmem cache */
 
 	struct kmem_cache;
 
@@ -123,9 +124,12 @@ extern uint64_t physmem;
     void kmem_cache_destroy(kmem_cache_t *cache);
     void *kmem_cache_alloc(kmem_cache_t *cache, int flags);
     void kmem_cache_free(kmem_cache_t *cache, void *buf);
+    void kmem_cache_free_to_slab(kmem_cache_t *cache, void *buf);
     void kmem_cache_reap_now(kmem_cache_t *cache);
+	void kmem_depot_ws_zero(kmem_cache_t *cache);
     void kmem_reap(void);
 	void kmem_reap_idspace(void);
+	kmem_cache_t *kmem_cache_buf_in_cache(kmem_cache_t *, void *);
 
     int kmem_debugging(void);
     void kmem_cache_set_move(kmem_cache_t *,
@@ -138,6 +142,7 @@ extern uint64_t physmem;
 	char *kmem_strstr(const char *in, const char *str);
 	void strident_canon(char *s, size_t n);
 
+boolean_t spl_arc_no_grow(size_t, boolean_t, kmem_cache_t **);
 
 #ifdef	__cplusplus
 }

openzfsonwindows/ZFSin

ZFSin/spl/include/sys/kmem.h
@@ -104,6 +104,7 @@ extern uint64_t physmem;
 #define KMC_KMEM_ALLOC  0x00200000      /* internal use only */
 #define KMC_IDENTIFIER  0x00400000      /* internal use only */
 #define KMC_PREFILL     0x00800000
+#define KMC_ARENA_SLAB  0x01000000      /* use a bigger kmem cache */
 
 	struct kmem_cache;
 
@@ -129,9 +130,12 @@ extern uint64_t physmem;
     void kmem_cache_destroy(kmem_cache_t *cache);
     void *kmem_cache_alloc(kmem_cache_t *cache, int flags);
     void kmem_cache_free(kmem_cache_t *cache, void *buf);
+    void kmem_cache_free_to_slab(kmem_cache_t *cache, void *buf);
     void kmem_cache_reap_now(kmem_cache_t *cache);
+	void kmem_depot_ws_zero(kmem_cache_t *cache);
     void kmem_reap(void);
 	void kmem_reap_idspace(void);
+	kmem_cache_t *kmem_cache_buf_in_cache(kmem_cache_t *, void *);
 
     int kmem_debugging(void);
     void kmem_cache_set_move(kmem_cache_t *,
@@ -143,6 +147,8 @@ extern uint64_t physmem;
     char *kmem_vasprintf(const char *fmt, va_list ap);
 	char *kmem_strstr(const char *in, const char *str);
 	void strident_canon(char *s, uint32_t n);
+
+	boolean_t spl_arc_no_grow(uint32_t, boolean_t, kmem_cache_t **);
 
 #ifdef	__cplusplus
 }