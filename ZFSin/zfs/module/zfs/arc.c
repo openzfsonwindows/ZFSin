ffaff00e11c3cfeef268a6482d95d2d065601714
Tom Caputi <tcaputi@datto.com>
2018-05-02 09:41:13 +0900

    Fix issues found with zfs diff
    Two deadlocks / ASSERT failures were introduced in a2c2ed1b which
    would occur whenever arc_buf_fill() failed to decrypt a block of
    data. This occurred because the call to arc_buf_destroy() which
    was responsible for cleaning up the newly created buffer would
    attempt to take out the hdr lock that it was already holding. This
    was resolved by calling the underlying functions directly without
    retaking the lock.
    
    In addition, the dmu_diff() code did not properly ensure that keys
    were loaded and mapped before begining dataset traversal. It turns
    out that this code does not need to look at any encrypted values,
    so the code was altered to perform raw IO only.
    
    Reviewed by: Matthew Ahrens <mahrens@delphix.com>
    Reviewed-by: Brian Behlendorf <behlendorf1@llnl.gov>
    Signed-off-by: Tom Caputi <tcaputi@datto.com>

--- ZFSin/zfs/module/zfs/arc.c
+++ ZFSin/zfs/module/zfs/arc.c
@@ -2297,8 +2297,10 @@ arc_buf_fill(arc_buf_t *buf, spa_t *spa, uint64_t dsobj, arc_fill_flags_t flags)
 	if (HDR_PROTECTED(hdr)) {
 		error = arc_fill_hdr_crypt(hdr, hash_lock, spa,
 		   dsobj, !!(flags & ARC_FILL_NOAUTH));
-		if (error != 0)
+		if (error != 0) {
+			arc_hdr_set_flags(hdr, ARC_FLAG_IO_ERROR);
 			return (error);
+		}
  	}
 
 	/*
@@ -2401,6 +2403,7 @@ arc_buf_fill(arc_buf_t *buf, spa_t *spa, uint64_t dsobj, arc_fill_flags_t flags)
 				    "hdr %p, compress %d, psize %d, lsize %d",
 				    hdr, arc_hdr_get_compress(hdr),
 				    HDR_GET_PSIZE(hdr), HDR_GET_LSIZE(hdr));
+				arc_hdr_set_flags(hdr, ARC_FLAG_IO_ERROR);
 				return (SET_ERROR(EIO));
 			}
 		}
@@ -5896,10 +5899,19 @@ arc_read_done(zio_t *zio)
 		/* This is a demand read since prefetches don't use callbacks */
 		callback_cnt++;
 
+		if (zio->io_error != 0)
+			continue;
+
 		int	error = arc_buf_alloc_impl(hdr, zio->io_spa,
 		    acb->acb_dsobj, acb->acb_private, acb->acb_encrypted,
 		    acb->acb_compressed, acb->acb_noauth, no_zio_error,
 		    &acb->acb_buf);
+		if (error != 0) {
+			(void) remove_reference(hdr, hash_lock,
+			    acb->acb_private);
+			arc_buf_destroy_impl(acb->acb_buf);
+			acb->acb_buf = NULL;
+		}
 
 		/*
 		 * Assert non-speculative zios didn't fail because an
@@ -6144,7 +6156,9 @@ top:
 				    spa, NULL, zb, NULL, 0, 0);
 			}
 			if (rc != 0) {
-				arc_buf_destroy(buf, private);
+				(void) remove_reference(hdr, hash_lock,
+				    private);
+				arc_buf_destroy_impl(buf);
 				buf = NULL;
 			}
 
