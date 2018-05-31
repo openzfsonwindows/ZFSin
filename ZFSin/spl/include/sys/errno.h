7093f4ca9023da8baf2d19e60c04baef20a7a5a6
Jorgen Lundman <lundman@lundman.net>
2018-05-31 12:18:03 +0900

    Kernel errno and userland did not match
    The errno values between userland and kernel were not always agreeing,
    which meant ENOTSUP would not be recognised and cause abort().
    
    We could look at including the userland errno.h in the kernel, but it is
    unlikely that is allowed.
    
    Related to issue #36 where setting compression=lz4 should have returned:
    
    cannot set property for 'tank': pool and or dataset must be upgraded to set this property or value
    
    instead of calling abort().
    
    However, why the pool thinks it is missing features is the real problem.

--- ZFSin/spl/include/sys/errno.h
+++ ZFSin/spl/include/sys/errno.h
@@ -80,7 +80,6 @@ extern "C" {
 #define	ENFILE	23	/* File table overflow			*/
 #define	EMFILE	24	/* Too many open files			*/
 #define	ENOTTY	25	/* Inappropriate ioctl for device	*/
-#define	ETXTBSY	26	/* Text file busy			*/
 #define	EFBIG	27	/* File too large			*/
 #define	ENOSPC	28	/* No space left on device		*/
 #define	ESPIPE	29	/* Illegal seek				*/
@@ -89,53 +88,64 @@ extern "C" {
 #define	EPIPE	32	/* Broken pipe				*/
 #define	EDOM	33	/* Math arg out of domain of func	*/
 #define	ERANGE	34	/* Math result not representable	*/
-#define	ENOMSG	35	/* No message of desired type		*/
-#define	EIDRM	36	/* Identifier removed			*/
-#define	ECHRNG	37	/* Channel number out of range		*/
-#define	EL2NSYNC 38	/* Level 2 not synchronized		*/
-#define	EL3HLT	39	/* Level 3 halted			*/
-#define	EL3RST	40	/* Level 3 reset			*/
-#define	ELNRNG	41	/* Link number out of range		*/
-#define	EUNATCH 42	/* Protocol driver not attached		*/
-#define	ENOCSI	43	/* No CSI structure available		*/
-#define	EL2HLT	44	/* Level 2 halted			*/
-#define	EDEADLK	45	/* Deadlock condition.			*/
-#define	ENOLCK	46	/* No record locks available.		*/
-#define	ECANCELED 47	/* Operation canceled			*/
-#define	ENOTSUP	48	/* Operation not supported		*/
 
-/* Filesystem Quotas */
-#define	EDQUOT	49	/* Disc quota exceeded			*/
+#define EDEADLK	36
+#define ENAMETOOLONG	38
+#define ENOLCK	39
+#define ENOSYS	40
+#define ENOTEMPTY	41
+#define EILSEQ	42              /* Illegal byte sequence */
 
-/* Convergent Error Returns */
-#define	EBADE	50	/* invalid exchange			*/
-#define	EBADR	51	/* invalid request descriptor		*/
-#define	EXFULL	52	/* exchange full			*/
-#define	ENOANO	53	/* no anode				*/
-#define	EBADRQC	54	/* invalid request code			*/
-#define	EBADSLT	55	/* invalid slot				*/
-#define	EDEADLOCK 56	/* file locking deadlock error		*/
+#define EDQUOT	49      /* Disc quota exceeded                  */
+#define EBADE	50      /* invalid exchange                     */
+#define ESHUTDOWN	58              /* Can't send after socket shutdown */
+#define ESTALE	70              /* Stale NFS file handle */
 
-#define ESHUTDOWN       58              /* Can't send after socket shutdown */
-
-#define ENAMETOOLONG    63              /* File name too long */
-
-#define ENOTEMPTY       66              /* Directory not empty */
-
-#define ESTALE          70              /* Stale NFS file handle */
-
-#define ENOSYS          78              /* Function not implemented */
-
-#define EOVERFLOW       84              /* Value too large to be stored in data type */
 #ifndef _KERNEL
-#define ERESTART    85  /* Interrupted system call should be restarted */
+#define ERESTART	85  /* Interrupted system call should be restarted */
 #endif
 
-#define EILSEQ          92              /* Illegal byte sequence */
-
-#define EALREADY 114 /* Operation already in progress */
-
-#define EOPNOTSUPP       ENOTSUP  
+#define EADDRINUSE      100
+#define EADDRNOTAVAIL   101
+#define EAFNOSUPPORT    102
+#define EALREADY        103
+#define EBADMSG         104
+#define ECANCELED       105
+#define ECONNABORTED    106
+#define ECONNREFUSED    107
+#define ECONNRESET      108
+#define EDESTADDRREQ    109
+#define EHOSTUNREACH    110
+#define EIDRM           111
+#define EINPROGRESS     112
+#define EISCONN         113
+#define ELOOP           114
+#define EMSGSIZE        115
+#define ENETDOWN        116
+#define ENETRESET       117
+#define ENETUNREACH     118
+#define ENOBUFS         119
+#define ENODATA         120
+#define ENOLINK         121
+#define ENOMSG          122
+#define ENOPROTOOPT     123
+#define ENOSR           124
+#define ENOSTR          125
+#define ENOTCONN        126
+#define ENOTRECOVERABLE 127
+#define ENOTSOCK        128
+#define ENOTSUP         129
+#define EOPNOTSUPP      130
+#define EOTHER          131
+#define EOVERFLOW       132
+#define EOWNERDEAD      133
+#define EPROTO          134
+#define EPROTONOSUPPORT 135
+#define EPROTOTYPE      136
+#define ETIME           137
+#define ETIMEDOUT       138
+#define ETXTBSY         139
+#define EWOULDBLOCK     140
 
 #ifdef	__cplusplus
 }
