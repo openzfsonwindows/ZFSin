566e42379763a75b666eb39df8193a5476ad8e6f
Paul Dagnelie <pcd@delphix.com>
2020-10-08 18:58:33 +0530

    Decrease contention on dn_struct_rwlock
    Signed-off-by: Paul Dagnelie <pcd@delphix.com>
    External-issue: DLPX-52564
    External-issue: DLPX-53085
    External-issue: DLPX-57384
    Ported by: Yash Jain <yash.jain@datacore.com>

--- ZFSin/zfs/include/sys/dmu_zfetch.h
+++ ZFSin/zfs/include/sys/dmu_zfetch.h
@@ -66,8 +66,8 @@ void		zfetch_fini(void);
 
 void		dmu_zfetch_init(zfetch_t *, struct dnode *);
 void		dmu_zfetch_fini(zfetch_t *);
-void		dmu_zfetch(zfetch_t *, uint64_t, uint64_t, boolean_t);
-
+void		dmu_zfetch(zfetch_t *, uint64_t, uint64_t, boolean_t,
+    boolean_t);
 
 #ifdef	__cplusplus
 }
